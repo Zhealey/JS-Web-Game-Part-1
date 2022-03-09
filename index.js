595b6b5fb9a5bedec2f15945381c9b6684849d1e
ZHealey <zackhealey@yahoo.com>
2022-03-09 18:21:37 -0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (source, x, y) {
+    let Image = document.createElement('img')
+    Image.src = source
+    Image.style.position = 'fixed'
+    Image.style.left   = x + 'px'
+    Image.style.bottom = y + 'px'
+    document.body.append(Image)
+    return Image
+}
+
+function newItem (source, x, y) {
+    let Item = newImage(source, x, y)
+    Item.addEventListener('dblclick', function() {
+        Item.remove()
+    })
+    return Item
+}
+
+let greenChar = newImage('assets/green-character.gif', 100, 100)
+let pineTree = newImage('assets/pine-tree.png', 450, 200)
+let tree = newImage('assets/tree.png', 200, 300)
+let pillar = newImage('assets/pillar.png', 350, 100)
+let crate = newImage('assets/crate.png', 150, 200)
+let well = newImage('assets/well.png', 500, 425)
+let sword  = newItem('assets/sword.png',  500, 405)
+let shield = newItem('assets/shield.png', 165, 185)
+let staff  = newItem('assets/staff.png',  600, 100)
